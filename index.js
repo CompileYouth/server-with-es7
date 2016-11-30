a2968602049370478d8a7cf6403c16d74b804617
CompileYouth <CompileYouth@163.com>
2016-11-30 15:28:59 +0800

    Add server.js.

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
-(async () => {
-    console.log("Hello babel");
-})()
+import httpServer from "./lib/http/server.js";
+
+const port = 8000;
+httpServer.listen(port, () => {
+    console.log(`The server is listening on port: ${port}`);
+});
