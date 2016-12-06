cde91d012ab5937e5642f4490ad0e606608a8c68
CompileYouth <CompileYouth@163.com>
2016-12-06 11:07:44 +0800

    Integrate with config and some refactor :tada:

--- index.js
+++ index.js
@@ -1,8 +1,35 @@
+import config from "config";
+
 import app from "./lib/app.js";
 import httpServer from "./lib/http/server.js";
 
-const port = 8000;
-app.attach(httpServer);
-httpServer.listen(port, () => {
-    console.log(`The server is listening on port: ${port}`);
-});
+
+
+(function main() {
+    checkEnv();
+    startHttpServer();
+})();
+
+function checkEnv() {
+    const env = app.get("env");
+    if (env === "development") {
+        console.log("dev mode");
+    }
+    else if (env === "production"){
+        console.log("production mode");
+    }
+    else {
+        throw new Error("wrong mode");
+    }
+}
+
+function startHttpServer() {
+    const port = parseInt(config.get("http.port"));
+    if (isNaN(port)) {
+        throw new Error(`"http.port" must be specified as number in config.`);
+    }
+    app.attach(httpServer);
+    httpServer.listen(port, () => {
+        console.log(`The server is listening on port: ${port}`);
+    });
+}
