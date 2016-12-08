2646414ab86b002ccba9ce1a915d4ed89dc52c1c
CompileYouth <CompileYouth@163.com>
2016-12-08 15:56:00 +0800

    Add pm2.

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,46 +7,34 @@ module.exports = {
 
     // First application
     {
-      name      : "API",
-      script    : "app.js",
+      name      : "SERVER",
+      max_memory_restart: "10M",
+      instances: 2,
+      script    : "index.js",
       env: {
-        COMMON_VARIABLE: "true"
+        "NODE_ENV": "development"
       },
-      env_production : {
-        NODE_ENV: "production"
-      }
+      exec_mode: "fork",
+      error_file: "./output/err.log",
+      out_file: "./output/info.log",
+      exec_interpreter: "./node_modules/.bin/babel-node",
+      merge_logs: true
     },
 
     // Second application
-    {
-      name      : "WEB",
-      script    : "web.js"
-    }
-  ],
-
-  /**
-   * Deployment section
-   * http://pm2.keymetrics.io/docs/usage/deployment/
-   */
-  deploy : {
-    production : {
-      user : "node",
-      host : "212.83.163.1",
-      ref  : "origin/master",
-      repo : "git@github.com:repo.git",
-      path : "/var/www/production",
-      "post-deploy" : "npm install && pm2 startOrRestart ecosystem.json --env production"
-    },
-    dev : {
-      user : "node",
-      host : "212.83.163.1",
-      ref  : "origin/master",
-      repo : "git@github.com:repo.git",
-      path : "/var/www/development",
-      "post-deploy" : "npm install && pm2 startOrRestart ecosystem.json --env dev",
-      env  : {
-        NODE_ENV: "dev"
-      }
-    }
-  }
+    // {
+    //   name      : "INS2",
+    //   max_memory_restart: "10M",
+    //   instances: 1,
+    //   script    : "index.js",
+    //   env: {
+    //     "NODE_ENV": "development",
+    //     "PORT": 8081
+    //   },
+    //   exec_mode: "fork",
+    //   error_file: "./output/err.log",
+    //   out_file: "./output/info.log",
+    //   exec_interpreter: "./node_modules/.bin/babel-node"
+    // }
+  ]
 }
