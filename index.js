d07e7fd58e7d96a464f37b32d9eb21e04d410f88
CompileYouth <CompileYouth@163.com>
2016-12-14 17:22:06 +0800

    Fix bug.

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ import app from "./lib/app.js";
 import httpServer from "./lib/http/server.js";
 
 app.attach(httpServer);
-httpServer.on(8081, () => {
+httpServer.listen(8081, () => {
     console.log(`The server is on port 8081...`);
 });
