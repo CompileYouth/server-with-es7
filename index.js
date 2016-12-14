1142967019b25f555e4d0ae190242f29960a204a
CompileYouth <CompileYouth@163.com>
2016-12-14 17:08:36 +0800

    Clear project for another exercise.

--- index.js
+++
@@ -1,9 +0,0 @@
-import app from "./lib/app.js";
-import httpServer from "./lib/http/server.js";
-
-app.attach(httpServer);
-
-const port = 8080;
-httpServer.listen(port, () => {
-    console.log(`The server is running on port ${port}...`);
-});
